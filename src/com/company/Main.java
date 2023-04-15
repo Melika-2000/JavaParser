cd3991b56b44058418576788c493dba894da6a3b
MelikaZ <uchiha.7990@gmail.com>
2023-04-16 00:26:24 +0430

    methods implementation improved, iteration through files added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,37 +1,38 @@
 package com.company;
 
-import com.github.javaparser.JavaParser;
+import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        FileInputStream inputStream = new FileInputStream("C:\\Users\\MelikaZ\\IdeaProjects\\OO_Proj\\src\\com\\company\\Temp.java");
-        JavaParser parser = new JavaParser();
-        CompilationUnit cu = parser.parse(inputStream).getResult().orElseThrow();
-        ClassOrInterfaceDeclaration classDeclaration = cu.getClassByName("Temp").get();
+        String projectP1 = "C:\\Users\\MelikaZ\\Documents\\Term 8\\OO Programming\\project\\4 - Netbeans v1.0.x\\src\\org\\openide\\TopManager.java";
+        ArrayList<File> classFile = getClassesAsFiles(projectP1);
+        CompilationUnit cu = StaticJavaParser.parse(classFile.get(0));
+        String className = getClassName(classFile.get(0));
+        ClassOrInterfaceDeclaration classDeclaration = cu.getClassByName(className).get();
 
         //
         String packageName = cu.getPackageDeclaration().get().getNameAsString();
-        String className = classDeclaration.getNameAsString();
         String classType = getType(classDeclaration);
         String classVisibility = getVisibility(classDeclaration);
-        List<Class<?>> children = getChildren(Temp.class);
-        ArrayList fields = getFields(Temp.class.getDeclaredFields());
-        ArrayList constructors = getConstructors(Temp.class);
+        List<SimpleName> children = getChildren(cu, className);
+        List<String> fields = getFields(classDeclaration);
+        String constructors = getConstructors(classDeclaration);
 
         boolean isStatic = classDeclaration.isStatic();
         boolean isAbstract = classDeclaration.isAbstract();
@@ -77,85 +78,72 @@ public class Main {
         return "Ordinary";
     }
 
-    public static List<Class<?>> getChildren(Class<?> parent) {
-        List<Class<?>> children = new ArrayList<>();
-        Package parentPackage = parent.getPackage();
-        String packageName = parentPackage.getName();
-        for (Class<?> clazz : getClasses(packageName)) {
-            if (parent.isAssignableFrom(clazz) && !parent.equals(clazz)) {
-                children.add(clazz);
+    public static List<SimpleName> getChildren(CompilationUnit cu, String className){
+        ClassOrInterfaceDeclaration currentClass = cu.getClassByName(className).get();
+        List<SimpleName> childClasses = new ArrayList<>();
+        cu.findAll(ClassOrInterfaceDeclaration.class).forEach(classDeclaration -> {
+            if (classDeclaration.getExtendedTypes().stream()
+                    .anyMatch(ext -> ext.getNameAsString().equals(currentClass.getNameAsString()))) {
+                childClasses.add(classDeclaration.getName());
             }
-        }
-        return children;
+        });
+        return childClasses;
     }
 
-    public static ArrayList getConstructors(Class<Temp> clazz){
-        ArrayList result = new ArrayList();
-        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
-        for (Constructor<?> constructor : constructors) {
-            int modifiers = constructor.getModifiers();
-            String modifierStr = Modifier.toString(modifiers);
-            result.add(modifierStr + " " + constructor.getName() + "(" + getParameterTypes(constructor) + ")");
+    public static String getConstructors(ClassOrInterfaceDeclaration classDeclaration){
+        String result = "";
+        List<ConstructorDeclaration> constructors = classDeclaration.getConstructors();
+        for (ConstructorDeclaration constructor : constructors) {
+            String name = constructor.getName().asString();
+            NodeList<com.github.javaparser.ast.body.Parameter> parameters = constructor.getParameters();
+            result += "[" + name + " , Parameters: ";
+            for (Parameter parameter : parameters) {
+                String paramName = parameter.getName().asString();
+                String paramType = parameter.getType().asString();
+                result += paramType + " " + paramName + " , ";
+            }
+            result += "]";
         }
         return result;
     }
 
-    private static ArrayList getFields(Field[] declaredFields){
-        Field[] allFields = declaredFields;
-        ArrayList names = new ArrayList<>();
-        Arrays.stream(allFields).forEach(field -> names.add(field.getName()));
-        return names;
+    public static List<String> getFields(ClassOrInterfaceDeclaration classDeclaration) {
+        List<String> fieldList = new ArrayList<>();
+        List<FieldDeclaration> fields = classDeclaration.getFields();
+        for (FieldDeclaration field : fields) {
+            String type = field.getCommonType().toString();
+            String name = field.getVariable(0).getNameAsString();
+            fieldList.add(type + " " + name);
+        }
+        return fieldList;
     }
 
-    private static List<Class<?>> getClasses(String packageName) {
-        List<Class<?>> classes = new ArrayList<>();
-        try {
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            String path = packageName.replace('.', '/');
-            for (java.net.URL resource : java.util.Collections.list(classLoader.getResources(path))) {
-                for (String file : new java.io.File(resource.toURI()).list()) {
-                    if (file.endsWith(".class")) {
-                        String className = packageName + '.' + file.substring(0, file.length() - 6);
-                        Class<?> clazz = Class.forName(className);
-                        classes.add(clazz);
-                    }
+    public static ArrayList<File> getClassesAsFiles(String path){
+        File file = new File(path);
+        Stack<File> s = new Stack<>();
+        ArrayList<File> files = new ArrayList<>();
+        s.push(file);
+        while (!s.empty()) {
+            File tmpF = s.pop();
+            if (tmpF.isFile() && tmpF.getName().endsWith(".java")) {
+                files.add(tmpF);
+            } else if (tmpF.isDirectory()) {
+                File[] f = tmpF.listFiles();
+                for (File fpp : f) {
+                    s.push(fpp);
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        return classes;
+        return files;
     }
-    private static String getParameterTypes(Constructor<?> constructor) {
-        Class<?>[] parameterTypes = constructor.getParameterTypes();
-        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < parameterTypes.length; i++) {
-            sb.append(parameterTypes[i].getSimpleName());
-
-            if (i < parameterTypes.length - 1) {
-                sb.append(", ");
-            }
-        }
-        return sb.toString();
+    public static String getClassName(File classFile){
+        return classFile.getName().replace(".java", "").replace('$', '.');
     }
 }
 
 
 
 
-/**
- *         File folder = new File("C:\\Users\\MelikaZ\\Documents\\Term 8\\OO Programming\\project\\5 - JUnit v3.7\\src\\junit\\tests");
- *         File[] listOfFiles = folder.listFiles(new FilenameFilter() {
- *             @Override
- *             public boolean accept(File dir, String name) {
- *                 return name.toLowerCase().endsWith(".java");
- *             }
- *         });
- *
- *         for (File file : listOfFiles) {
- *             if (file.isFile()) {
- *                 System.out.println(file.getName());
- *             }
- *         }
- */
+
+
