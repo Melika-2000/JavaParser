053b3cd295005c3fae2cb47eaa6de5916555edbb
MelikaZ <uchiha.7990@gmail.com>
2023-06-03 23:04:44 +0430

    RequestMapping detection added to MicroserviceInfo

--- src/com/company/MicroserviceInfo.java
+++ src/com/company/MicroserviceInfo.java
@@ -22,25 +22,33 @@ public class MicroserviceInfo {
     private void getApiMethods(ClassOrInterfaceDeclaration classDeclaration) {
         for (MethodDeclaration method : classDeclaration.getMethods()) {
 
-            if(method.getAnnotationByName("POST").isPresent()){
+            if(method.getAnnotationByName("POST").isPresent() ||
+                    (method.getAnnotationByName("RequestMapping").isPresent() &&
+                            method.getAnnotations().toString().contains("POST"))){
                 String methodName = method.getName().asString() +
                         " - Parameters: " + method.getParameters() ;
                 postMethods.add(methodName);
             }
 
-            else if(method.getAnnotationByName("PUT").isPresent()){
+            else if(method.getAnnotationByName("PUT").isPresent() ||
+                    (method.getAnnotationByName("RequestMapping").isPresent() &&
+                            method.getAnnotations().toString().contains("PUT"))){
                 String methodName = method.getName().asString() +
                         " - Parameters: " + method.getParameters() + " ";
                 putMethods.add(methodName);
             }
 
-            else if(method.getAnnotationByName("GET").isPresent()){
+            else if(method.getAnnotationByName("GET").isPresent() ||
+                    (method.getAnnotationByName("RequestMapping").isPresent() &&
+                            method.getAnnotations().toString().contains("GET"))){
                 String methodName = method.getName().asString() +
                         " - Parameters: " + method.getParameters() + " ";
                 getMethods.add(methodName);
             }
 
-            else if(method.getAnnotationByName("DELETE").isPresent()){
+            else if(method.getAnnotationByName("DELETE").isPresent() ||
+                    (method.getAnnotationByName("RequestMapping").isPresent() &&
+                            method.getAnnotations().toString().contains("DELETE"))){
                 String methodName = method.getName().asString() +
                         " - Parameters: " + method.getParameters() + " " ;
                 deleteMethods.add(methodName);
